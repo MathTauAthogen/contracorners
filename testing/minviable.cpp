742a7364577d19a44eba09c1cc18a71941f48155
Chilli Qin <wqin2008@gmail.com>
2024-12-07 13:20:17 -0500

    Checkpoint - delayed due to business

--- testing/minviable.cpp
+++ testing/minviable.cpp
@@ -1,4 +1,16 @@
+class ImageDrawable : public Drawable < ImageDrawable > {
 
+	public:
+
+		int derived_var = 1534;
+
+		ImageDrawable ();
+
+
+		Pixel get_pixel ( int x, int y );
+};
+
+#endif
 
 
 int main () {
